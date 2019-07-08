109c53ce0e6e6a3347abf6d5b958b43731f65c53
pier <nmall843r>
2019-07-08 12:01:46 -0700

    refactoring and synthesis implementation

--- goal_model.py
+++ goal_model.py
@@ -1,17 +1,27 @@
+from contracts.contract_operations import *
+import itertools
 
-class GoalContract():
+
+class GoalModel:
 
     def __init__(self, name, contracts,
-                 sub_goals=[],
+                 sub_goals=None,
                  sub_operation=None,
-                 parent_goal=[],
+                 parent_goal=None,
                  parent_operation=None):
 
+        if parent_goal is None:
+            parent_goal = []
+        if sub_goals is None:
+            sub_goals = []
         self.name = name
 
         # Contracts is a list of Assumption/Guarantee pairs
         # Each element of the list is in conjunction with each other
-        self.contracts = contracts
+        if not isinstance(contracts, list):
+            self.contracts = [contracts]
+        else:
+            self.contracts = contracts
 
         # List of children and its relationship with them (COMPOSITION / CONJUNCTION)
         self.sub_goals = sub_goals
@@ -42,3 +52,82 @@ class GoalContract():
 
     def get_contracts(self):
         return self.contracts
+
+
+class NotComposableError(Exception):
+    """
+    raised if it is not possible to compose
+    """
+    pass
+
+
+class NotConjoinableError(Exception):
+    """
+    raised if it is not possible to conjoin
+    """
+    pass
+
+
+def compose_goals(goals, name=None):
+    """
+
+    :param name: Name of the goal
+    :param goals: List of goals to compose
+    :return: composed_goals if success otherwise
+    """
+
+    contracts = {}
+
+    for goal in goals:
+        contracts[goal.get_name()] = goal.get_contracts()
+
+    if name is None:
+        name = '_'.join("{!s}".format(key) for (key, val) in contracts.items())
+
+    composition_contracts = (dict(zip(contracts, x)) for x in itertools.product(*contracts.itervalues()))
+
+    composed_contract_list = []
+    for contracts in composition_contracts:
+        satis, composed_contract = compose_contracts(contracts)
+        if not satis:
+            print("composition failed")
+            raise NotComposableError
+        composed_contract_list.append(composed_contract)
+
+    # Creating a new Goal parent
+    composed_goal = GoalModel(name, composed_contract_list,
+                              sub_goals=goals,
+                              sub_operation="COMPOSITION")
+
+    # Connecting children to the parent
+    for goal in goals:
+        goal.set_parent(composed_goal, "COMPOSITION")
+
+    return composed_goal
+
+
+def conjoin_goals(goals, name):
+    conjoined_contracts = []
+
+    for goal in goals:
+        conjoined_contracts.append(goal.get_contracts())
+
+    # Flattening list
+    conjoined_contracts = [item for sublist in conjoined_contracts for item in sublist]
+
+    # TODO: fix checks on conjunction
+    # sat = conjoin_contracts(conjoined_contracts)
+    # if not sat:
+    #     print("conjoin failed"
+    #     return False, None
+
+    # Creating a new Goal parent
+    conjoined_goal = GoalModel(name, conjoined_contracts,
+                               sub_goals=goals,
+                               sub_operation="CONJUNCTION")
+
+    # Connecting children to the parent
+    for goal in goals:
+        goal.set_parent(conjoined_goal, "CONJUNCTION")
+
+    return conjoined_goal
--- utility.py
+++
@@ -1,6 +0,0 @@
-
-
-def merge_two_dicts(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
